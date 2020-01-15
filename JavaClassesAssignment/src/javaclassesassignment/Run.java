d91c5e5f7b485433882f25b815a02b693d015d67
Paul Breakthrough <paulbreakthru@outlook.com>
2020-01-15 11:44:01 +0100

    InitialConfiguration

---
+++ JavaClassesAssignment/src/javaclassesassignment/Run.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaclassesassignment;
+
+/**
+ *
+ * @author Breakthru
+ */
+public class Run {
+    public void HundredMeters(){
+        System.out.println("This is a hundred meters race");
+    }
+    public void CrossCountry(){
+        System.out.println("This is a cross country race");
+    }
+}
